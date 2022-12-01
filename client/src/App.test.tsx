5ffeaea8b044640d4883393efb4f8f7db84af33f
Li Andrey <li.andr1771@gmail.com>
2022-12-01 12:16:58 +0600

    tests

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import FirstComponent from './components/FirstComponent';
 
-test('renders learn react link', () => {
+test('renders hakuna', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/hakuna/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders react logo', () => {
+  render(<App />);
+  const imgElement = screen.getByAltText(/logo/i);
+  expect(imgElement).toBeInTheDocument();
+})
+
+test('renders FirstComponent', () => {
+  render(<FirstComponent />);
+  const textElement = screen.getByText(/FirstComponent/i);
+  expect(textElement).toBeInTheDocument();
+})
---
+++ client/src/components/FirstComponent.tsx
@@ -0,0 +1,17 @@
+import React, { SyntheticEvent } from 'react'
+
+type FirstComponentProps = {
+
+}
+
+const FirstComponent: React.FC = (props: FirstComponentProps) => {
+
+
+    return (
+        <div>
+            <h1> FirstComponent </h1>
+        </div>
+    )
+}
+
+export default FirstComponent
