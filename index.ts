27c3864d09c2effaaaac4f0a73682234941e9573
li.andr1771@gmail.com <li.andr1771@gmail.com>
2022-12-26 19:19:48 +0600

    mongodb queries

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import dotenv from "dotenv";
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
 import { TApplic } from './types/TApplic';
+import { MongoClient } from "mongodb";
+import mongoose from "mongoose";
 
 dotenv.config();
 
@@ -10,162 +12,139 @@ const app: Express = express();
 app.use(express.json());
 app.use(cors());
 
-let applics: TApplic[] = [
-    {
-        "user_id": 1,
-        "fio": "Тест Тестович",
-        "fio_eng": "Test Testovich",
-        "birth_date": "2000-01-01",
-        "sex": "male",
-        "post_address": "Moscow",
-        "email": "test@test.com",
-        "phones": "+77777777",
-        "edu_institute": "MGU",
-        "edu_institute_address": "Moscow",
-        "edu_specialization": "Programmist",
-        "uch_stepen": "Magister",
-        "work_company": "Test&Co",
-        "work_start_year": 2022,
-        "work_department": "IT",
-        "work_position": "Junior spicialist",
-        "work_specialization": "Frontend",
-        "public_organizations": "test",
-        "antok_city": "test",
-        "science_interests": "test",
-        "conf_topic": "test",
-        "conf_section": "test",
-        "conf_coauthors": "test",
-        "participation_type": "test",
-        "created_at": "2012-01-26T13:51:50.417Z",
-        "updated_at": "2012-01-26T13:51:50.417Z",
-        "need_compensation": false,
-        "inn": "test",
-        "snils": "test",
-        "registration": "test",
-        "phone_work": "test",
-        "phone_home": "test",
-        "uch_zvanie": "test",
-        "work_city": "test",
-        "work_country": "test",
-        "acad_position": "test",
-        "work_company_short": "test",
-        "need_hotel": "test",
-        "birth_year": 2000
-    },
-    {
-        "user_id": 2,
-        "fio": "Тестa Тестовична",
-        "fio_eng": "Testa Testovichna",
-        "birth_date": "2000-01-01",
-        "sex": "female",
-        "post_address": "Moscow",
-        "email": "testa@test.com",
-        "phones": "+78888888",
-        "edu_institute": "MGU",
-        "edu_institute_address": "Moscow",
-        "edu_specialization": "Programmist",
-        "uch_stepen": "Magister",
-        "work_company": "Test&Co",
-        "work_start_year": 2022,
-        "work_department": "IT",
-        "work_position": "Junior spicialist",
-        "work_specialization": "Frontend",
-        "public_organizations": "test",
-        "antok_city": "test",
-        "science_interests": "test",
-        "conf_topic": "test",
-        "conf_section": "test",
-        "conf_coauthors": "test",
-        "participation_type": "test",
-        "created_at": "2012-01-26T13:51:50.417Z",
-        "updated_at": "2012-01-26T13:51:50.417Z",
-        "need_compensation": false,
-        "inn": "test",
-        "snils": "test",
-        "registration": "test",
-        "phone_work": "test",
-        "phone_home": "test",
-        "uch_zvanie": "test",
-        "work_city": "test",
-        "work_country": "test",
-        "acad_position": "test",
-        "work_company_short": "test",
-        "need_hotel": "test",
-        "birth_year": 2000
-    }
-]
+
+// MongoDB
+// Connection URI
+const uri =
+  "mongodb://localhost:27017";
+
+// Create a new MongoClient
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+
+    // Establish and verify connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Connected successfully to server");
+    
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 // GET 
 app.get("/api/applics", async (req: Request, res: Response) => {
     try {
+    await client.connect()
+        const database = client.db("antok")
+        const applicsCol = database.collection<TApplic>("applics")
+        const cursor = applicsCol.find()
+        const applics = await cursor.toArray()
         return res.json(applics)
     }
     catch (err) {
         console.log("ERROR", err)
+    }
+    finally{
+    await client.close()
     }
 })
 
 // POST
 app.post("/api/applics", async (req: Request, res: Response) => {
     try {
+        await client.connect()
+        const database = client.db("antok")
+        const applicsCol = database.collection<TApplic>("applics")
         const body = req.body
-        const userId = applics.length + 1;
         const newApplic: TApplic = {
-            ...body,
-            user_id: userId,
-
+            ...body
         }
-        applics = applics.concat(newApplic)
-        res.json(newApplic)
+        const result = await applicsCol.insertOne(newApplic);
+       return res.json(result)
     }
     catch (err) {
         res.status(400).json({
             error: 'content missing'
         })
     }
+    finally{
+        await client.close()
+        }
 })
 
 // GET :id
 app.get("/api/applics/:id", async (req: Request, res: Response) => {
     try {
-        const id = Number(req.params.id)
-        const applic = applics.find(app => app.user_id === id)
+        await client.connect()
+        const database = client.db("antok")
+        const applicsCol = database.collection<TApplic>("applics")
+        const id = req.params.id
+        
+        const query = { _id: new mongoose.Types.ObjectId(id)} 
+        const applic = await applicsCol.findOne<TApplic>(query)
         return res.json(applic)
     }
     catch (err) {
         console.log("ERROR", err)
     }
+    finally{
+        await client.close()
+        }
 })
 
 // PATCH
 app.patch('/api/applics/:id', async (req: Request, res: Response) => {
     try {
+        await client.connect()
+        const database = client.db("antok")
+        const applicsCol = database.collection<TApplic>("applics")
         const body = req.body
-        const updatedApplic: TApplic = {
-            ...body,
+        const id = req.params.id
+
+        const filter = { _id: new mongoose.Types.ObjectId(id)} 
+        const updatedApplic = {
+            _id: new mongoose.Types.ObjectId(id),
+         ...body
         }
-        applics = applics.map((a) => a.user_id !== updatedApplic.user_id ? a : updatedApplic)
+        await applicsCol.replaceOne(filter, updatedApplic);
         res.json(updatedApplic)
     }
     catch (err) {
         console.log("ERROR", err)
-    }
+    }    
+    finally{
+        await client.close()
+        }
 })
 
 // DELETE
 app.delete("/api/applics/:id", async (req: Request, res: Response) => {
     try {
-        const id = Number(req.params.id)
-        applics = applics.filter((a) => a.user_id !== id)
+        await client.connect()
+        const database = client.db("antok")
+        const applicsCol = database.collection("applics")
+        const id = req.params.id
+        const query = { _id: new mongoose.Types.ObjectId(id)} 
+        await applicsCol.deleteOne(query);
         return res.status(204).end()
     }
     catch (err) {
         console.log("ERROR", err)
     }
+    finally{
+        await client.close()
+        }
 })
 
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8005;
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
